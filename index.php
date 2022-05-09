b98ba316051dde950e520853b9f9d8f7257335a7
syafiqmk <syafiqmuh.k@gmail.com>
2022-05-09 18:46:42 +0800

    update index.php

--- index.php
+++ index.php
@@ -13,3 +13,16 @@
     //include title
     include "title.php";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php title($page) ?></title>
+</head>
+<body>
+    
+</body>
+</html>
