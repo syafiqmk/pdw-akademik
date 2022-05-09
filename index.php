f2d4e3c6a9ba20089c4e3a1eb04f5a60f13902eb
syafiqmk <syafiqmuh.k@gmail.com>
2022-05-09 18:40:16 +0800

    update index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php 
-    
+    // include koneksi
+    include "koneksi.php";
+
+    //user check
+    if (!$_SESSION["user"]) {
+        header("location:page/login.php");
+    }
+
+    //get variable
+    $page = $_GET["page"];
+
+    //include title
+    include "title.php";
 ?>
